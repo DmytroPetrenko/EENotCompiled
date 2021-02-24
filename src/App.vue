3f54e7db6465c64c77c005c7a7878466b3c6944c
dmytropetrenko <dmitry.a.petrenko@gmail.com>
2021-02-24 10:58:29 +0200

    StartProject

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<HeaderComponent></HeaderComponent>
+		<router-view></router-view>
+	</div>
 </template>
 
-<style>
+<script>
+import HeaderComponent from "./components/layout/HeaderComponent.vue"
+
+export default {
+	name: "app",
+	components: {
+		HeaderComponent,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
