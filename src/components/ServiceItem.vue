2422abfd1a188c4f6e199800f514d68582f8c7c4
dmytropetrenko <dmitry.a.petrenko@gmail.com>
2021-03-01 11:53:41 +0200

    last change

---
+++ src/components/ServiceItem.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator"
+import { Item } from "@/types"
+
+@Component
+export default class ServiceItem extends Vue {
+	@Prop() item?: Item
+}
+</script>
+
+<template>
+	<el-col :span="4">
+		<img :src="item.images[item.id]" alt="" />
+		<h4>{{ $t("service.services.service" + item.id + ".h") }}</h4>
+		<p>{{ $t("service.services.service" + item.id + ".p") }}</p>
+	</el-col>
+</template>
