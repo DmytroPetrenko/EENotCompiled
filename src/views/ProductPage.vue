322690ff158de47ea5008b34df10a11245238925
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-05-07 19:08:02 +0300

    img down position

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,11 +1,18 @@
 <template>
 	<el-row type="flex" justify="center">
 		<el-col :span="8">
-			<el-carousel :autoplay="false" trigger="click" indicator-position="outside">
-				<el-carousel-item v-for="image in product.images" :key="image">
-					<img :src="require(`@/assets/img/${image}`)" class="image" />
-				</el-carousel-item>
-			</el-carousel>
+			<el-row>
+				<el-carousel :autoplay="false" trigger="click" indicator-position="none">
+					<el-carousel-item v-for="image in product.images" :key="image">
+						<img :src="require(`@/assets/img/${image}`)" class="image" />
+					</el-carousel-item>
+				</el-carousel>
+			</el-row>
+			<el-row class="suggested-image">
+				<div class="image" v-for="image in product.images" :key="image">
+					<img :src="require(`@/assets/img/${image}`)" />
+				</div>
+			</el-row>
 		</el-col>
 		<el-col :span="8">2</el-col>
 	</el-row>
@@ -34,26 +41,36 @@ export default class ProductPage extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.el-carousel__indicators--outside button {
-	width: 14px;
-	height: 14px;
-	border-radius: 14px;
-	background-color: rgb(255, 0, 0);
-}
-.el-carousel-item {
-	position: relative;
+.el-carousel {
+	.el-carousel-item {
+		position: relative;
+	}
+
+	.image {
+		max-height: 100%;
+		max-width: 100%;
+		width: auto;
+		height: auto;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		margin: auto;
+	}
 }
 
-.image {
-	max-height: 100%;
-	max-width: 100%;
-	width: auto;
-	height: auto;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	margin: auto;
+.suggested-image {
+	margin: 5px 0;
+	width: 100%;
+	.image {
+		max-width: 100px;
+		height: 100%;
+		float: left;
+		margin: 0 5px;
+		img {
+			width: 100%;
+		}
+	}
 }
 </style>
