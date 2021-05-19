83af71fbf408215e26aca684c2669099ba630eec
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-05-19 11:49:16 +0300

    service css improvement

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,57 +1,60 @@
 <template>
 	<div class="service">
-		<el-row type="flex" class="row-bg" justify="center">
-			<el-col :span="16">
-				<p>{{ $t("service.p1") }}</p>
-			</el-col>
+		<el-row :gutter="10">
+			<el-col :xs="24" :sm="{ span: 20, offset: 2 }" :lg="{ span: 16, offset: 4 }"
+				><div>
+					<p>{{ $t("service.p1") }}</p>
+				</div></el-col
+			>
 		</el-row>
-		<el-row type="flex" class="row-bg" justify="center">
-			<el-col :span="4">
-				<img src="@/assets/img/maintenance.svg" alt="" />
-				<h4>{{ $t("service.services.service1.h") }}</h4>
-				<p>{{ $t("service.services.service1.p") }}</p>
-			</el-col>
-			<el-col :span="4">
-				<img src="@/assets/img/high-voltage.svg" alt="" />
-				<h4>{{ $t("service.services.service2.h") }}</h4>
-				<p>{{ $t("service.services.service2.p") }}</p>
-			</el-col>
-			<el-col :span="4">
-				<img src="@/assets/img/spring.svg" alt="" />
-				<h4>{{ $t("service.services.service1.h") }}</h4>
-				<p>{{ $t("service.services.service1.p") }}</p>
-			</el-col>
-			<el-col :span="4">
-				<img src="@/assets/img/airbag.svg" alt="" />
-				<h4>{{ $t("service.services.service2.h") }}</h4>
-				<p>{{ $t("service.services.service2.p") }}</p>
-			</el-col>
+		<el-row :gutter="10">
+			<el-col :xs="24" :sm="{ span: 10, offset: 2 }" :lg="{ span: 4, offset: 4 }"
+				><div>
+					<img src="@/assets/img/maintenance.svg" alt="" />
+					<h4>{{ $t("service.services.service1.h") }}</h4>
+					<p>{{ $t("service.services.service1.p") }}</p>
+				</div></el-col
+			>
+			<el-col :xs="24" :sm="10" :lg="4"
+				><div>
+					<img src="@/assets/img/high-voltage.svg" alt="" />
+					<h4>{{ $t("service.services.service2.h") }}</h4>
+					<p>{{ $t("service.services.service2.p") }}</p>
+				</div></el-col
+			>
+			<el-col :xs="24" :sm="{ span: 10, offset: 2 }" :lg="{ span: 4, offset: 0 }"
+				><div>
+					<img src="@/assets/img/spring.svg" alt="" />
+					<h4>{{ $t("service.services.service3.h") }}</h4>
+					<p>{{ $t("service.services.service3.p") }}</p>
+				</div></el-col
+			>
+			<el-col :xs="24" :sm="10" :md="10" :lg="4"
+				><div>
+					<img src="@/assets/img/airbag.svg" alt="" />
+					<h4>{{ $t("service.services.service4.h") }}</h4>
+					<p>{{ $t("service.services.service4.p") }}</p>
+				</div></el-col
+			>
 		</el-row>
-		<el-row type="flex" class="row-bg" justify="center">
-			<el-col :span="16">
-				<img id="mainImg" src="@/assets/img/main.jpg" alt="photo" />
-			</el-col>
+		<el-row :gutter="10">
+			<el-col :xs="24" :sm="{ span: 20, offset: 2 }" :lg="{ span: 16, offset: 4 }"
+				><div>
+					<img id="mainImg" src="@/assets/img/main.jpg" alt="photo" /></div
+			></el-col>
 		</el-row>
-		<el-row type="flex" class="row-bg" justify="center">
-			<el-col :span="16">
-				<p>{{ $t("service.p2") }}</p>
-			</el-col>
+		<el-row :gutter="10">
+			<el-col :xs="24" :sm="{ span: 20, offset: 2 }" :lg="{ span: 16, offset: 4 }"
+				><div>
+					<p>{{ $t("service.p2") }}</p>
+				</div></el-col
+			>
 		</el-row>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-.el-row {
-	flex-wrap: wrap;
-	margin-bottom: 20px;
-	&:last-child {
-		margin-bottom: 0;
-	}
-}
 .el-col {
-	border-radius: 4px;
-	min-width: 250px;
-	padding: 0 20px;
 	h4 {
 		margin: 0;
 	}
